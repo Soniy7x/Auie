5291abd021677163da71ffbc380cca48847771a1
葛相池 <soniy7x@gmail.com>
2014-10-22 14:47:48 +0800

    Add - 注释类（布局、控件、对象、空）

---
+++ Auie/src/com/deliration/auie/annotation/UEWidget.java
@@ -0,0 +1,15 @@
+package com.deliration.auie.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UEWidget {
+	int ID() default -1;
+	boolean onClick() default false;
+}
