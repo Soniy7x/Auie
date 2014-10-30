177dc16f9714339bb8bd1a75755e39ff4375668c
Soniy7x <soniy7x@gmail.com>
2014-10-30 16:24:55 +0800

    Fixed - 视图滑动卡UISwictherViewPager更名为UIViewPager 修复禁滑无效

---
+++ Auie/src/org/auie/ui/UIViewPager.java
@@ -0,0 +1,49 @@
+package org.auie.ui;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class UIViewPager extends ViewPager{
+	
+	private boolean enableScroll = false;
+	
+    public UIViewPager(Context context) {  
+        super(context);  
+    }  
+  
+    public UIViewPager(Context context, AttributeSet attrs) {  
+        super(context, attrs);
+    }
+
+    public boolean isEnableScroll() {
+		return enableScroll;
+	}
+
+	public void setEnableScroll(boolean enableScroll) {
+		this.enableScroll = enableScroll;
+	}
+
+	/**
+     * 禁止响应滑动事件
+     */
+	@SuppressLint("ClickableViewAccessibility")
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		if (!enableScroll) {			
+			return true;
+		}
+		return super.onTouchEvent(event);
+	}
+
+	@Override
+	public void scrollTo(int x, int y) {
+		if (enableScroll) {
+			super.scrollTo(x, y);
+		}
+	}
+    
+	
+}
