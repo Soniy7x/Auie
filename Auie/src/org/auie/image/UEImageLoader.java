51c338b940983554e474f312b5e97040ae80cbe0
Soniy7x <soniy7x@gmail.com>
2014-11-05 16:55:39 +0800

    Update - 增加异步加载本地图片文件

--- Auie/src/org/auie/image/UEImageLoader.java
+++ Auie/src/org/auie/image/UEImageLoader.java
@@ -1,5 +1,6 @@
 package org.auie.image;
 
+import java.io.IOException;
 import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -8,6 +9,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.auie.core.Auie;
+import org.auie.utils.UEImageNotByteException;
 
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -16,107 +18,94 @@ import android.util.Log;
 
 public class UEImageLoader {
 
-	//保存正在下载的图片URL集合，避免重复下载用  
-    private static HashSet<String> sDownloadingSet;  
-    //软引用内存缓存  
-    private static Map<String,SoftReference<Bitmap>> sImageCache;   
-    //图片三种获取方式管理者，网络URL获取、内存缓存获取、外部文件缓存获取  
-    private static UEImageLoaderHelper impl;  
-    //线程池相关  
-    private static ExecutorService sExecutorService;  
-      
-    //通知UI线程图片获取ok时使用  
-    private Handler handler;   
-      
-      
-    /** 
-     * 异步加载图片完毕的回调接口 
-     */  
-    public interface ImageCallback{  
-        /** 
-         * 回调函数 
-         * @param bitmap: may be null! 
-         * @param imageUrl  
-         */  
-        public void onImageLoaded(Bitmap bitmap, String imageUrl);  
-    }  
-      
+    private static HashSet<String> mSet;
+    private static Map<String,SoftReference<Bitmap>> mImageCache;   
+    private static UEImageCacheManager cacheManager;  
+    private static ExecutorService mExecutorService;
+    private Handler mHandler;   
+    
     static{  
-        sDownloadingSet = new HashSet<String>();  
-        sImageCache = new HashMap<String,SoftReference<Bitmap>>();  
-        impl = new UEImageLoaderHelper(sImageCache);  
+        mSet = new HashSet<String>();  
+        mImageCache = new HashMap<String,SoftReference<Bitmap>>();  
+        cacheManager = new UEImageCacheManager(mImageCache);  
     }  
   
     public UEImageLoader(Context context){  
-        handler = new Handler();  
-        startThreadPoolIfNecessary();  
-          
+        mHandler = new Handler();  
+        startThreadPoolIfNecessary();
         String defaultDir = context.getCacheDir().getAbsolutePath();  
         setCachedDir(defaultDir);  
     }  
-      
-    /** 
-     * 是否缓存图片至/data/data/package/cache/目录 
-     * 默认不缓存 
-     */  
+       
     public void setCache2File(boolean flag){  
-        impl.setCache2File(flag);  
+        cacheManager.setCache2File(flag);  
     }  
-      
-    /** 
-     * 设置缓存路径，setCache2File(true)时有效 
-     */  
+    
     public void setCachedDir(String dir){  
-        impl.setCachedDir(dir);  
+        cacheManager.setCachedDir(dir);  
     }  
   
-    /**开启线程池*/  
     public static void startThreadPoolIfNecessary(){  
-        if(sExecutorService == null || sExecutorService.isShutdown() || sExecutorService.isTerminated()){  
-            sExecutorService = Executors.newFixedThreadPool(3);  
-            //sExecutorService = Executors.newSingleThreadExecutor();  
+        if(mExecutorService == null || mExecutorService.isShutdown() || mExecutorService.isTerminated()){  
+            mExecutorService = Executors.newFixedThreadPool(3);
         }  
     }  
-      
-    /** 
-     * 异步下载图片，并缓存到memory中 
-     * @param url    
-     * @param callback  see ImageCallback interface 
-     */  
-    public void downloadImage(final String url, final ImageCallback callback){  
+    
+    public void downloadImage(final String url, final UEImageLoadListener callback){  
         downloadImage(url, true, callback);  
     }  
       
-    /** 
-     *  
-     * @param url 
-     * @param cache2Memory 是否缓存至memory中 
-     * @param callback 
-     */  
-    public void downloadImage(final String url, final boolean cache2Memory, final ImageCallback callback){  
-        if(sDownloadingSet.contains(url)){  
+    public void downloadFile(final String url, final UEImageLoadListener callback){  
+    	Bitmap bitmap = cacheManager.getBitmapFromMemory(url);  
+        if(bitmap != null){  
+            if(callback != null){  
+                callback.onImageLoadComlepeted(bitmap, url);  
+            }  
+        }else{  
+            mExecutorService.submit(new Runnable(){  
+                @Override  
+                public void run() {  
+					try {
+						final Bitmap bitmap = new UEImage(url, true).toBitmap();
+						mHandler.post(new Runnable(){  
+							@Override  
+							public void run(){  
+								if(callback != null)  
+									callback.onImageLoadComlepeted(bitmap, url);  
+								mSet.remove(url);  
+							}  
+						});  
+					} catch (IOException | UEImageNotByteException e) {
+						e.printStackTrace();
+					}  
+                }  
+            });  
+        }  
+    }
+    
+    public void downloadImage(final String url, final boolean cache2Memory, final UEImageLoadListener callback){  
+        if(mSet.contains(url)){  
             Log.w(Auie.TAG, "图片正在下载，不能重复下载");  
             return;
         }  
           
-        Bitmap bitmap = impl.getBitmapFromMemory(url);  
+        Bitmap bitmap = cacheManager.getBitmapFromMemory(url);  
         if(bitmap != null){  
             if(callback != null){  
-                callback.onImageLoaded(bitmap, url);  
+                callback.onImageLoadComlepeted(bitmap, url);  
             }  
-        }else{  
-            //从网络端下载图片  
-            sDownloadingSet.add(url);  
-            sExecutorService.submit(new Runnable(){  
+        }else{
+            mSet.add(url);  
+            mExecutorService.submit(new Runnable(){  
                 @Override  
                 public void run() {  
-                    final Bitmap bitmap = impl.getBitmapFromUrl(url, cache2Memory);  
-                    handler.post(new Runnable(){  
+                    final Bitmap bitmap = cacheManager.getBitmapFromHttp(url, cache2Memory);  
+                    mHandler.post(new Runnable(){  
                         @Override  
                         public void run(){  
                             if(callback != null)  
-                                callback.onImageLoaded(bitmap, url);  
-                            sDownloadingSet.remove(url);  
+                                callback.onImageLoadComlepeted(bitmap, url);  
+                            mSet.remove(url);  
                         }  
                     });  
                 }  
@@ -124,12 +113,13 @@ public class UEImageLoader {
         }  
     }  
       
-    /** 
-     * 预加载下一张图片，缓存至memory中 
-     * @param url  
-     */  
-    public void preLoadNextImage(final String url){  
-        //将callback置为空，只将bitmap缓存到memory即可。  
+    public void preLoadNextImage(final String url){    
         downloadImage(url, null);  
     }  
+    
+    
+    public interface UEImageLoadListener{   
+        public void onImageLoadComlepeted(Bitmap bitmap, String imageUrl);  
+    }  
+      
 }
