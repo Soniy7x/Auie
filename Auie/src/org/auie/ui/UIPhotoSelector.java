03304d76d64cd8d4cb2bbc8141eb3e2199015506
Soniy7x <soniy7x@gmail.com>
2014-11-06 16:53:54 +0800

    Add - 照片选择器UIPhotoSelector

---
+++ Auie/src/org/auie/ui/UIPhotoSelector.java
@@ -0,0 +1,427 @@
+package org.auie.ui;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.auie.image.UEImageLoader;
+import org.auie.image.UEImageManager;
+import org.auie.image.UEImageLoader.OnUEImageLoadListener;
+import org.auie.image.UEImageManager.Bucket;
+import org.auie.image.UEImageManager.Image;
+import org.auie.utils.UEAdapter;
+import org.auie.utils.UEImageNotByteException;
+import org.auie.utils.UEMethod;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.text.TextUtils.TruncateAt;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView.ScaleType;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+
+@SuppressWarnings("deprecation")
+public class UIPhotoSelector extends PopupWindow {
+	
+	private static final int MATCH_PARENT1 = LinearLayout.LayoutParams.MATCH_PARENT;
+	private static final int MATCH_PARENT2 = RelativeLayout.LayoutParams.MATCH_PARENT;
+	
+	private Context context;
+	private GridView photoGridView;
+	private ListView bucketListView;
+	private LinearLayout rootContainer;
+	private RelativeLayout topContainer;
+	private UIButton topLeftButton;
+	private UIButton topRightButton;
+	private RelativeLayout bottomContainer;
+	private UIButton bottomLeftButton;
+	private UIButton bottomRightButton;
+	private View bottomLineView;
+	
+	private int WIDTH = 0;
+	private int HEIGHT = 0;
+	private int IMAGE_SIZE = 0;
+	private int DP = 1;
+	
+	private List<Image> selectImages = new ArrayList<>();
+	private List<Image> images;
+	private List<Bucket> buckets;
+	private UEImageManager manager;
+	private ImageAdapter adapter;
+	private BucketAdapter bucketAdapter;
+	
+	private OnUIPhotoSelectorListener selectorListener;
+	
+	public UIPhotoSelector(Context context, OnUIPhotoSelectorListener listener) {
+		WindowManager manager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		WindowManager.LayoutParams attrs = ((Activity) context).getWindow().getAttributes();
+		this.context = context;
+		this.selectorListener = listener;
+		this.DP = UEMethod.dp2px(context, 1);
+		if((attrs.flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) == WindowManager.LayoutParams.FLAG_FULLSCREEN){
+			this.HEIGHT = manager.getDefaultDisplay().getHeight();
+		}else {
+			this.HEIGHT = manager.getDefaultDisplay().getHeight() - 24 * DP;
+		}
+		this.WIDTH = manager.getDefaultDisplay().getWidth();
+		this.IMAGE_SIZE = (WIDTH - 4 * DP) / 3;
+		createView();
+	}
+	
+	private void createView(){
+		setBackgroundDrawable(new BitmapDrawable());
+		setContentView(createContentView());
+		setWidth(WIDTH);
+		setHeight(HEIGHT);
+		setFocusable(true);
+		initDatas();
+	}
+	
+	private void initDatas(){
+		manager = UEImageManager.getInstance(context);
+		buckets = manager.getTempBuckets(true);
+		bucketAdapter = new BucketAdapter(buckets);
+		bucketListView.setAdapter(bucketAdapter);
+		images = buckets.get(buckets.size() - 1).images;
+		adapter = new ImageAdapter(images);
+		photoGridView.setAdapter(adapter);
+	}
+	
+	private OnClickListener topLeftClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			if (selectorListener != null) {
+				selectorListener.onSelectCancel();
+			}
+			dismiss();
+		}
+	};
+	
+	private OnClickListener topRightClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			if (selectImages.size() > 0 && selectorListener != null) {
+				selectorListener.onSelectCompleted(selectImages);
+				dismiss();
+			}
+		}
+	};
+	
+	private OnClickListener bottomLeftClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			if (bucketListView.getVisibility() == View.GONE) {
+				bucketListView.setVisibility(View.VISIBLE);
+			}else {
+				bucketListView.setVisibility(View.GONE);
+			}
+		}
+	};
+	
+	private OnClickListener bottomRightClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			if (selectImages.size() > 0) {
+				try {
+					new UIImagePager(context, selectImages).show();
+				} catch (IOException | UEImageNotByteException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+	
+	private OnItemClickListener bucketOnItemClickListener = new OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			selectImages.clear();
+			adapter.refresh(buckets.get(position).images);
+			bucketListView.setVisibility(View.GONE);
+		}
+	};
+	
+	private OnItemClickListener photoOnItemClickListener = new OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			ImageView imageView = (ImageView) ((RelativeLayout)view).getChildAt(1);
+			if (imageView.getVisibility() == View.INVISIBLE) {
+				imageView.setVisibility(View.VISIBLE);
+				selectImages.add(images.get(position));
+			}else {
+				imageView.setVisibility(View.INVISIBLE);
+				selectImages.remove(images.get(position));
+			}
+			bottomRightButton.setText("预览(" + selectImages.size() + ")");
+			if (selectImages.size() > 0) {
+				bottomRightButton.setTextColor(Color.parseColor("#EFEFEF"));
+				topRightButton.setVisibility(View.VISIBLE);
+			}else {
+				bottomRightButton.setTextColor(Color.parseColor("#5F5F5F"));
+				topRightButton.setVisibility(View.GONE);
+			}
+		}
+	};
+	
+	private LinearLayout.LayoutParams getParams(int width, int height, int weight){
+        return new LinearLayout.LayoutParams(width, height, weight);
+    }
+	
+	private LinearLayout.LayoutParams getParams1(int width, int height){
+        return new LinearLayout.LayoutParams(width, height);
+    }
+	
+	private RelativeLayout.LayoutParams getParams2(int width, int height){
+        return new RelativeLayout.LayoutParams(width, height);
+    }
+	
+	private View createContentView(){
+		/**
+		 * 根视图容器
+		 */
+		rootContainer = new LinearLayout(context);
+		rootContainer.setLayoutParams(getParams1(MATCH_PARENT1, MATCH_PARENT1));
+		rootContainer.setBackgroundColor(Color.parseColor("#191919"));
+		rootContainer.setOrientation(LinearLayout.VERTICAL);
+		
+		/**
+		 * 顶栏
+		 */
+		topContainer = new RelativeLayout(context);
+		topContainer.setLayoutParams(getParams1(MATCH_PARENT1, 48 * DP));
+		topContainer.setBackgroundColor(Color.parseColor("#2e3334"));
+		
+		LayoutParams topLeftParams = (LayoutParams) getParams2(60 * DP, MATCH_PARENT2);
+		topLeftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+		topLeftParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+		topLeftButton = new UIButton(context);
+		topLeftButton.setLayoutParams(topLeftParams);
+		topLeftButton.setBackgroundColor(Color.parseColor("#2e3334"));
+		topLeftButton.setImageDrawable(context.getResources().getDrawable(android.R.drawable.ic_menu_revert));
+		topLeftButton.setOnClickListener(topLeftClickListener);
+		
+		LayoutParams topRightParams = (LayoutParams) getParams2(60 * DP, 30 * DP);
+		topRightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+		topRightParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+		topRightParams.setMargins(0, 8 * DP, 8 * DP, 0);
+		topRightButton = new UIButton(context);
+		topRightButton.setLayoutParams(topRightParams);
+		topRightButton.setTextSize(12);
+		topRightButton.setVisibility(View.GONE);
+		topRightButton.setText("完成");
+		topRightButton.setTextColor(Color.parseColor("#FFFFFF"));
+		topRightButton.setBackgroundColor(Color.parseColor("#00b58a"));
+		topRightButton.setOnClickListener(topRightClickListener);
+		
+		topContainer.addView(topLeftButton);
+		topContainer.addView(topRightButton);
+		
+		/**
+		 * 相片视图
+		 */
+		LinearLayout.LayoutParams photoParams = getParams(MATCH_PARENT1, 0, 1);
+		photoParams.setMargins(0, (int)(0.8 * DP), 0, (int)(0.8 * DP));
+		photoGridView = new GridView(context);
+		photoGridView.setLayoutParams(photoParams);
+		photoGridView.setBackgroundColor(Color.parseColor("#242424"));
+		photoGridView.setHorizontalSpacing(2 * DP);
+		photoGridView.setVerticalSpacing(2 * DP);
+		photoGridView.setNumColumns(3);
+		photoGridView.setVerticalScrollBarEnabled(false);
+		photoGridView.setOnItemClickListener(photoOnItemClickListener);
+		
+		/**
+		 * 底栏
+		 */
+		bottomContainer = new RelativeLayout(context);
+		bottomContainer.setLayoutParams(getParams1(MATCH_PARENT1, 48 * DP));
+		bottomContainer.setBackgroundColor(Color.parseColor("#2e3334"));
+		
+		LayoutParams bottomLeftParams = (LayoutParams) getParams2(90 * DP, MATCH_PARENT2);
+		bottomLeftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
+		bottomLeftParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+		bottomLeftButton = new UIButton(context);
+		bottomLeftButton.setLayoutParams(bottomLeftParams);
+		bottomLeftButton.setText("切换相册");
+		bottomLeftButton.setTextColor(Color.parseColor("#B8B8B8"));
+		bottomLeftButton.setBackgroundColor(Color.parseColor("#2e3334"));
+		bottomLeftButton.setOnClickListener(bottomLeftClickListener);
+		
+		LayoutParams bottomRightParams = (LayoutParams) getParams2(80 * DP, 30 * DP);
+		bottomRightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+		bottomRightParams.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
+		bottomRightButton = new UIButton(context);
+		bottomRightButton.setId(1992);
+		bottomRightButton.setText("预览");
+		bottomRightButton.setTextColor(Color.parseColor("#5F5F5F"));
+		bottomRightButton.setBackgroundColor(Color.parseColor("#2e3334"));
+		bottomRightButton.setLayoutParams(bottomRightParams);
+		bottomRightButton.setOnClickListener(bottomRightClickListener);
+		
+		LayoutParams bottomLineParams = (LayoutParams) getParams2((int)(0.8 * DP), MATCH_PARENT2);
+		bottomLineParams.addRule(RelativeLayout.LEFT_OF, 1992);
+		bottomLineParams.setMargins(0, 8 * DP, 0, 8 * DP);
+		bottomLineView = new View(context);
+		bottomLineView.setLayoutParams(bottomLineParams);
+		bottomLineView.setBackgroundColor(Color.parseColor("#555555"));
+		
+		
+		bottomContainer.addView(bottomLeftButton);
+		bottomContainer.addView(bottomRightButton);
+		bottomContainer.addView(bottomLineView);
+		
+		/**
+		 * 相册视图
+		 */
+		LinearLayout.LayoutParams bucketParams = getParams1(MATCH_PARENT1, 278 * DP);
+		bucketParams.setMargins(0, (int)(0.8 * DP), 0, 0);
+		bucketListView = new ListView(context);
+		bucketListView.setLayoutParams(bucketParams);
+		bucketListView.setBackgroundColor(Color.parseColor("#F8F8F8"));
+		bucketListView.setVisibility(View.GONE);
+		bucketListView.setOnItemClickListener(bucketOnItemClickListener);
+		
+		rootContainer.addView(topContainer);
+		rootContainer.addView(photoGridView);
+		rootContainer.addView(bottomContainer);
+		rootContainer.addView(bucketListView);
+		
+		return rootContainer;
+	}
+	
+	public UIPhotoSelector show(){
+		showAtLocation(((ViewGroup)(((Activity) context).findViewById(android.R.id.content))).getChildAt(0), Gravity.BOTTOM, 0, 0);
+		return this;
+	}
+	
+	class BucketAdapter extends UEAdapter{
+
+		public BucketAdapter(List<?> datas) {
+			super(datas);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Holder holder;
+			if (convertView == null) {
+				holder = new Holder();
+				LinearLayout mContainerLayout = new LinearLayout(context);
+				mContainerLayout.setLayoutParams((new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT,AbsListView.LayoutParams.WRAP_CONTENT)));
+				mContainerLayout.setPadding(16 * DP,10 * DP, 16 * DP, 10 * DP);
+				mContainerLayout.setGravity(Gravity.CENTER_VERTICAL);
+				mContainerLayout.setOrientation(LinearLayout.HORIZONTAL);
+				holder.icon = new ImageView(context);
+				holder.icon.setLayoutParams(getParams1(72 * DP, 72 * DP));
+				holder.icon.setScaleType(ScaleType.CENTER_CROP);
+				LinearLayout.LayoutParams params = getParams1(MATCH_PARENT1, MATCH_PARENT1);
+				params.setMargins(16 * DP, 0, 0, 0);
+				holder.name = new TextView(context);
+				holder.name.setLayoutParams(params);
+				holder.name.setEllipsize(TruncateAt.END);
+				holder.name.setSingleLine(true);
+				holder.name.setTextSize(16);
+				holder.name.setTextColor(Color.parseColor("#888888"));
+				holder.name.setGravity(Gravity.CENTER_VERTICAL);
+				mContainerLayout.addView(holder.icon);
+				mContainerLayout.addView(holder.name);
+				convertView = mContainerLayout;
+				convertView.setTag(holder);
+			}else {
+				holder = (Holder) convertView.getTag();
+			}
+			Bucket bucket = (Bucket) getItem(position);
+			holder.name.setText(bucket.name + "(" + bucket.count +")");
+			Image image = bucket.images.get(0);
+			final String thumbPath = image.thumbnail;
+			final String path = image.path;
+			if (thumbPath == null && path == null) {
+				return convertView;
+			}
+			final ImageView ImageView = holder.icon;
+			new UEImageLoader(context).downloadFile(thumbPath == null ? path : thumbPath, new OnUEImageLoadListener() {
+				
+				@Override
+				public void onImageLoadComlepeted(Bitmap bitmap, String imageUrl) {
+					ImageView.setImageBitmap(bitmap);
+				}
+			});
+			return convertView;
+		}
+		
+		class Holder {
+			private ImageView icon;
+			private TextView name;
+		}
+	}
+	
+	class ImageAdapter extends UEAdapter{
+
+		public ImageAdapter(List<?> datas) {
+			super(datas);
+		}
+
+		@SuppressLint("InflateParams")
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView imageView;
+			if (convertView == null) {
+				RelativeLayout mContainer = new RelativeLayout(context);
+				mContainer.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.WRAP_CONTENT,AbsListView.LayoutParams.WRAP_CONTENT));
+				imageView = new ImageView(context);
+				imageView.setLayoutParams(getParams2(IMAGE_SIZE, IMAGE_SIZE));
+				imageView.setScaleType(ScaleType.CENTER_CROP);
+				RelativeLayout.LayoutParams params = getParams2(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+				params.setMargins(4 * DP, 4 * DP, 4 * DP, 4 * DP);
+				params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, RelativeLayout.TRUE);
+				ImageView selectImageView = new ImageView(context);
+				selectImageView.setLayoutParams(params);
+				selectImageView.setVisibility(View.INVISIBLE);
+				selectImageView.setImageResource(android.R.drawable.presence_online);
+				mContainer.addView(imageView);
+				mContainer.addView(selectImageView);
+				convertView = mContainer;
+				convertView.setTag(imageView);
+			}else {
+				imageView = (ImageView) convertView.getTag();
+			}
+			String path = ((Image)getItem(position)).thumbnail;
+			if (path == null) {
+				path = ((Image)getItem(position)).path;
+			}
+			if (path == null) {
+				return convertView;
+			}
+			final ImageView iv = imageView;
+			new UEImageLoader(context).downloadFile(path, new OnUEImageLoadListener() {
+				@Override
+				public void onImageLoadComlepeted(Bitmap bitmap, String imageUrl) {
+					iv.setImageBitmap(bitmap);
+				}
+			});
+			return convertView;
+		}
+		
+	}
+	
+	public interface OnUIPhotoSelectorListener{
+		public void onSelectCompleted(List<Image> images);
+		public void onSelectCancel();
+	}
+}
