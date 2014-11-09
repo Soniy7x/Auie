a2d1d0a77dbf65c3343e65bdfafe319102b70486
Soniy7x <soniy7x@gmail.com>
2014-11-09 20:10:05 +0800

    Add - UIGridView网格视图(支持获取onMeasure状态)

---
+++ Auie/src/org/auie/ui/UIGridView.java
@@ -0,0 +1,50 @@
+package org.auie.ui;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+@TargetApi(Build.VERSION_CODES.L)
+public class UIGridView extends GridView {
+	
+	private boolean measure = false;
+	
+	public UIGridView(Context context) {
+		super(context);
+	}
+
+	public UIGridView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public UIGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	public UIGridView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+		super(context, attrs, defStyleAttr, defStyleRes);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		measure = true;
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		measure = false;
+		super.onLayout(changed, l, t, r, b);
+	}
+
+	public boolean isMeasure() {
+		return measure;
+	}
+
+	public void setMeasure(boolean measure) {
+		this.measure = measure;
+	}
+
+}
--- Auie/src/org/auie/utils/UETag.java
+++
@@ -1,7 +0,0 @@
-package org.auie.utils;
-
-public class UETag {
-	
-	public static final String TAG = "Auie";
-	
-}
