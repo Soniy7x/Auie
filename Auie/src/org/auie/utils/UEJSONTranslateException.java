8c98f8695bb67ff59b8d898b64175730402ce301
Soniy7x <soniy7x@gmail.com>
2014-11-10 13:16:24 +0800

    Add - UEJSONTranslateException JSON数据转换异常

---
+++ Auie/src/org/auie/utils/UEJSONTranslateException.java
@@ -0,0 +1,22 @@
+package org.auie.utils;
+
+public class UEJSONTranslateException extends Throwable {
+
+	private static final long serialVersionUID = 1L;
+
+	public UEJSONTranslateException() {
+        super();
+    }
+    
+    public UEJSONTranslateException(String msg) {
+        super(msg);
+    }
+    
+    public UEJSONTranslateException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+    
+    public UEJSONTranslateException(Throwable cause) {
+        super(cause);
+    }
+}
