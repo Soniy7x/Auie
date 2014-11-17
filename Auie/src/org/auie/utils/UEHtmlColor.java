52d765e819e3fbfb93bf054647f2dcc4866c73ff
Soniy7x <soniy7x@gmail.com>
2014-11-17 16:32:44 +0800

    Update - UEHtmlColor简化颜色调用

--- Auie/src/org/auie/utils/UEHtmlColor.java
+++ Auie/src/org/auie/utils/UEHtmlColor.java
@@ -1,158 +1,149 @@
 package org.auie.utils;
 
-import android.graphics.Color;
-
-public enum UEHtmlColor {
+public final class UEHtmlColor {
 	
-	ALIECEBLUE(Color.parseColor("#F0F8FF")),
-	ANTIQUEWHITE(Color.parseColor("#FAEBD7")),
-	AQUA(Color.parseColor("#00FFFF")),
-	AQUAMARINE(Color.parseColor("#7FFFD4")),
-	AZURE(Color.parseColor("#F0FFFF")),
-	BEIGE(Color.parseColor("#F5F5DC")),
-	BISQUE(Color.parseColor("#FFE4C4")),
-	BLACK(Color.parseColor("#000000")),
-	BLANCHEDALMOND(Color.parseColor("#FFEBCD")),
-	BLUE(Color.parseColor("#0000FF")),
-	BLUEVIOLET(Color.parseColor("#8A2BE2")),
-	BROWN(Color.parseColor("#A52A2A")),
-	BURLYWOOD(Color.parseColor("#DEB887")),
-	CADETBLUE(Color.parseColor("#5F9EA0")),
-	CHARTREUSE(Color.parseColor("#7FFF00")),
-	CHOCOLATE(Color.parseColor("#D2691E")),
-	CORAL(Color.parseColor("#FF7F50")),
-	CORNFLOWERBLUE(Color.parseColor("#6495ED")),
-	CORNSILK(Color.parseColor("#FFF8DC")),
-	CRIMSON(Color.parseColor("#DC143C")),
-	CYAN(Color.parseColor("#00FFFF")),
-	DARKBLUE(Color.parseColor("#00008B")),
-	DARKCYAN(Color.parseColor("#008B8B")),
-	DARKGOLDENROD(Color.parseColor("#B8860B")),
-	DARKGRAY(Color.parseColor("#A9A9A9")),
-	DARKGREEN(Color.parseColor("#006400")),
-	DARKKHAKI(Color.parseColor("#BDB76B")),
-	DARKMAGENTA(Color.parseColor("#8B008B")),
-	DARKOLIVEGREEN(Color.parseColor("#556B2F")),
-	DARKORANGE(Color.parseColor("#FF8C00")),
-	DARKORCHID(Color.parseColor("#9932CC")),
-	DARKRED(Color.parseColor("#8B0000")),
-	DARKSALMON(Color.parseColor("#E9967A")),
-	DARKSEAGREEN(Color.parseColor("#8FBC8F")),
-	DARKSLATEBLUE(Color.parseColor("#483D8B")),
-	DARKSLATEGRAY(Color.parseColor("#2F4F4F")),
-	DARKTURQUOISE(Color.parseColor("#00CED1")),
-	DARKVIOLET(Color.parseColor("#9400D3")),
-	DEEPPINK(Color.parseColor("#FF1493")),
-	DEEPSKYBLUE(Color.parseColor("#00BFFF")),
-	DIMGRAY(Color.parseColor("#696969")),
-	DODGERBLUE(Color.parseColor("#1E90FF")),
-	FIREBRICK(Color.parseColor("#B22222")),
-	FLORALWHITE(Color.parseColor("#FFFAF0")),
-	FORESTGREEN(Color.parseColor("#228B22")),
-	FUCHSIA(Color.parseColor("#FF00FF")),
-	GAINSBORO(Color.parseColor("#DCDCDC")),
-	GHOSTWHITE(Color.parseColor("#F8F8FF")),
-	GOLD(Color.parseColor("#FFD700")),
-	GOLDENROD(Color.parseColor("#DAA520")),
-	GRAY(Color.parseColor("#808080")),
-	GREEN(Color.parseColor("#008000")),
-	GREENYELLOW(Color.parseColor("#ADFF2F")),
-	HONEYDEW(Color.parseColor("#F0FFF0")),
-	HOTPINK(Color.parseColor("#FF69B4")),
-	INDIANRED(Color.parseColor("#CD5C5C")),
-	INDIGO(Color.parseColor("#4B0082")),
-	IVORY(Color.parseColor("#FFFFF0")),
-	KHAKI(Color.parseColor("#F0E68C")),
-	LAVENDER(Color.parseColor("#E6E6FA")),
-	LAVENDERBLUSH(Color.parseColor("#FFF0F5")),
-	LAWNGREEN(Color.parseColor("#7CFC00")),
-	LEMONCHIFFON(Color.parseColor("#FFFACD")),
-	LIGHTBLUE(Color.parseColor("#ADD8E6")),
-	LIGHTCORAL(Color.parseColor("#F08080")),
-	LIGHTCYAN(Color.parseColor("#E0FFFF")),
-	LIGHTGOLDENRODYELLOW(Color.parseColor("#FAFAD2")),
-	LIGHTGRAY(Color.parseColor("#D3D3D3")),
-	LIGHTGREEN(Color.parseColor("#90EE90")),
-	LIGHTPINK(Color.parseColor("#FFB6C1")),
-	LIGHTSALMON(Color.parseColor("#FFA07A")),
-	LIGHTSEAGREEN(Color.parseColor("#20B2AA")),
-	LIGHTSKYBLUE(Color.parseColor("#87CEFA")),
-	LIGHTSLATEGRAY(Color.parseColor("#778899")),
-	LIGHTSTEELBLUE(Color.parseColor("#B0C4DE")),
-	LIGHTYELLOW(Color.parseColor("#FFFFE0")),
-	LIME(Color.parseColor("#00FF00")),
-	LIMEGREEN(Color.parseColor("#32CD32")),
-	LINEN(Color.parseColor("#FAF0E6")),
-	MAGENTA(Color.parseColor("#FF00FF")),
-	MAROON(Color.parseColor("#800000")),
-	MEDIUMAQUAMARINE(Color.parseColor("#66CDAA")),
-	MEDIUMBLUE(Color.parseColor("#0000CD")),
-	MEDIUMORCHID(Color.parseColor("#BA55D3")),
-	MEDIUMPURPLE(Color.parseColor("#9370DB")),
-	MEDIUMSEAGREEN(Color.parseColor("#3CB371")),
-	MEDIUMSLATEBLUE(Color.parseColor("#7B68EE")),
-	MEDIUMSPRINGGREEN(Color.parseColor("#00FA9A")),
-	MEDIUMTURQUOISE(Color.parseColor("#48D1CC")),
-	MEDIUMVIOLETRED(Color.parseColor("#C71585")),
-	MIDNIGHTBLUE(Color.parseColor("#191970")),
-	MINTCREAM(Color.parseColor("#F5FFFA")),
-	MISTYROSE(Color.parseColor("#FFE4E1")),
-	MOCCASIN(Color.parseColor("#FFE4B5")),
-	NAVAJOWHITE(Color.parseColor("#FFDEAD")),
-	NAVY(Color.parseColor("#000080")),
-	OLDLACE(Color.parseColor("#FDF5E6")),
-	OLIVE(Color.parseColor("#808000")),
-	OLIVEDRAB(Color.parseColor("#6B8E23")),
-	ORANGE(Color.parseColor("#FFA500")),
-	ORANGERED(Color.parseColor("#FF4500")),
-	ORCHID(Color.parseColor("#DA70D6")),
-	PALEGOLDENROD(Color.parseColor("#EEE8AA")),
-	PALEGREEN(Color.parseColor("#98FB98")),
-	PALETURQUOISE(Color.parseColor("#AFEEEE")),
-	PALEVIOLETRED(Color.parseColor("#DB7093")),
-	PAPAYAWHIP(Color.parseColor("#FFEFD5")),
-	PEACHPUFF(Color.parseColor("#FFDAB9")),
-	PERU(Color.parseColor("#CD853F")),
-	PINK(Color.parseColor("#FFC0CB")),
-	PLUM(Color.parseColor("#DDA0DD")),
-	POWDERBLUE(Color.parseColor("#B0E0E6")),
-	PURPLE(Color.parseColor("#800080")),
-	RED(Color.parseColor("#FF0000")),
-	ROSYBROWN(Color.parseColor("#BC8F8F")),
-	ROYALBLUE(Color.parseColor("#4169E1")),
-	SADDLEBROWN(Color.parseColor("#8B4513")),
-	SALMON(Color.parseColor("#FA8072")),
-	SANDYBROWN(Color.parseColor("#F4A460")),
-	SEAGREEN(Color.parseColor("#2E8B57")),
-	SEASHELL(Color.parseColor("#FFF5EE")),
-	SIENNA(Color.parseColor("#A0522D")),
-	SILVER(Color.parseColor("#C0C0C0")),
-	SKYBLUE(Color.parseColor("#87CEEB")),
-	SLATEBLUE(Color.parseColor("#6A5ACD")),
-	SLATEGRAY(Color.parseColor("#708090")),
-	SNOW(Color.parseColor("#FFFAFA")),
-	SPRINGGREEN(Color.parseColor("#00FF7F")),
-	STEELBLUE(Color.parseColor("#4682B4")),
-	TAN(Color.parseColor("#D2B48C")),
-	TEAL(Color.parseColor("#008080")),
-	THISTLE(Color.parseColor("#D8BFD8")),
-	TOMATO(Color.parseColor("#FF6347")),
-	TRANSPARENT(Color.parseColor("#00000000")),
-	TURQUOISE(Color.parseColor("#40E0D0")),
-	VIOLET(Color.parseColor("#EE82EE")),
-	WHEAT(Color.parseColor("#F5DEB3")),
-	WHITE(Color.parseColor("#FFFFFF")),
-	WHITESMOKE(Color.parseColor("#F5F5F5")),
-	YELLOW(Color.parseColor("#FFFF00")),
-	YELLOWGREEN(Color.parseColor("#9ACD32"));
-	
-	private int color;
+	public static final int ALIECEBLUE = 0xFFF0F8FF;
+	public static final int ANTIQUEWHITE = 0xFFFAEBD7;
+	public static final int AQUA = 0xFF00FFFF;
+	public static final int AQUAMARINE = 0xFF7FFFD4;
+	public static final int AZURE = 0xFFF0FFFF;
+	public static final int BEIGE = 0xFFF5F5DC;
+	public static final int BISQUE = 0xFFFFE4C4;
+	public static final int BLACK = 0xFF000000;
+	public static final int BLANCHEDALMOND = 0xFFFFEBCD;
+	public static final int BLUE = 0xFF0000FF;
+	public static final int BLUEVIOLET = 0xFF8A2BE2;
+	public static final int BROWN = 0xFFA52A2A;
+	public static final int BURLYWOOD = 0xFFDEB887;
+	public static final int CADETBLUE = 0xFF5F9EA0;
+	public static final int CHARTREUSE = 0xFF7FFF00;
+	public static final int CHOCOLATE = 0xFFD2691E;
+	public static final int CORAL = 0xFFFF7F50;
+	public static final int CORNFLOWERBLUE = 0xFF6495ED;
+	public static final int CORNSILK = 0xFFFFF8DC;
+	public static final int CRIMSON = 0xFFDC143C;
+	public static final int CYAN = 0xFF00FFFF;
+	public static final int DARKBLUE = 0xFF00008B;
+	public static final int DARKCYAN = 0xFF008B8B;
+	public static final int DARKGOLDENROD = 0xFFB8860B;
+	public static final int DARKGRAY = 0xFFA9A9A9;
+	public static final int DARKGREEN = 0xFF006400;
+	public static final int DARKKHAKI = 0xFFBDB76B;
+	public static final int DARKMAGENTA = 0xFF8B008B;
+	public static final int DARKOLIVEGREEN = 0xFF556B2F;
+	public static final int DARKORANGE = 0xFFFF8C00;
+	public static final int DARKORCHID = 0xFF9932CC;
+	public static final int DARKRED = 0xFF8B0000;
+	public static final int DARKSALMON = 0xFFE9967A;
+	public static final int DARKSEAGREEN = 0xFF8FBC8F;
+	public static final int DARKSLATEBLUE = 0xFF483D8B;
+	public static final int DARKSLATEGRAY = 0xFF2F4F4F;
+	public static final int DARKTURQUOISE = 0xFF00CED1;
+	public static final int DARKVIOLET = 0xFF9400D3;
+	public static final int DEEPPINK = 0xFFFF1493;
+	public static final int DEEPSKYBLUE = 0xFF00BFFF;
+	public static final int DIMGRAY = 0xFF696969;
+	public static final int DODGERBLUE = 0xFF1E90FF;
+	public static final int FIREBRICK = 0xFFB22222;
+	public static final int FLORALWHITE = 0xFFFFFAF0;
+	public static final int FORESTGREEN = 0xFF228B22;
+	public static final int FUCHSIA = 0xFFFF00FF;
+	public static final int GAINSBORO = 0xFFDCDCDC;
+	public static final int GHOSTWHITE = 0xFFF8F8FF;
+	public static final int GOLD = 0xFFFFD700;
+	public static final int GOLDENROD = 0xFFDAA520;
+	public static final int GRAY = 0xFF808080;
+	public static final int GREEN = 0xFF008000;
+	public static final int GREENYELLOW = 0xFFADFF2F;
+	public static final int HONEYDEW = 0xFFF0FFF0;
+	public static final int HOTPINK = 0xFFFF69B4;
+	public static final int INDIANRED = 0xFFCD5C5C;
+	public static final int INDIGO = 0xFF4B0082;
+	public static final int IVORY = 0xFFFFFFF0;
+	public static final int KHAKI = 0xFFF0E68C;
+	public static final int LAVENDER = 0xFFE6E6FA;
+	public static final int LAVENDERBLUSH = 0xFFFFF0F5;
+	public static final int LAWNGREEN = 0xFF7CFC00;
+	public static final int LEMONCHIFFON = 0xFFFFFACD;
+	public static final int LIGHTBLUE = 0xFFADD8E6;
+	public static final int LIGHTCORAL = 0xFFF08080;
+	public static final int LIGHTCYAN = 0xFFE0FFFF;
+	public static final int LIGHTGOLDENRODYELLOW = 0xFFFAFAD2;
+	public static final int LIGHTGRAY = 0xFFD3D3D3;
+	public static final int LIGHTGREEN = 0xFF90EE90;
+	public static final int LIGHTPINK = 0xFFFFB6C1;
+	public static final int LIGHTSALMON = 0xFFFFA07A;
+	public static final int LIGHTSEAGREEN = 0xFF20B2AA;
+	public static final int LIGHTSKYBLUE = 0xFF87CEFA;
+	public static final int LIGHTSLATEGRAY = 0xFF778899;
+	public static final int LIGHTSTEELBLUE = 0xFFB0C4DE;
+	public static final int LIGHTYELLOW = 0xFFFFFFE0;
+	public static final int LIME = 0xFF00FF00;
+	public static final int LIMEGREEN = 0xFF32CD32;
+	public static final int LINEN = 0xFFFAF0E6;
+	public static final int MAGENTA = 0xFFFF00FF;
+	public static final int MAROON = 0xFF800000;
+	public static final int MEDIUMAQUAMARINE = 0xFF66CDAA;
+	public static final int MEDIUMBLUE = 0xFF0000CD;
+	public static final int MEDIUMORCHID = 0xFFBA55D3;
+	public static final int MEDIUMPURPLE = 0xFF9370DB;
+	public static final int MEDIUMSEAGREEN = 0xFF3CB371;
+	public static final int MEDIUMSLATEBLUE = 0xFF7B68EE;
+	public static final int MEDIUMSPRINGGREEN = 0xFF00FA9A;
+	public static final int MEDIUMTURQUOISE = 0xFF48D1CC;
+	public static final int MEDIUMVIOLETRED = 0xFFC71585;
+	public static final int MIDNIGHTBLUE = 0xFF191970;
+	public static final int MINTCREAM = 0xFFF5FFFA;
+	public static final int MISTYROSE = 0xFFFFE4E1;
+	public static final int MOCCASIN = 0xFFFFE4B5;
+	public static final int NAVAJOWHITE = 0xFFFFDEAD;
+	public static final int NAVY = 0xFF000080;
+	public static final int OLDLACE = 0xFFFDF5E6;
+	public static final int OLIVE = 0xFF808000;
+	public static final int OLIVEDRAB = 0xFF6B8E23;
+	public static final int ORANGE = 0xFFFFA500;
+	public static final int ORANGERED = 0xFFFF4500;
+	public static final int ORCHID = 0xFFDA70D6;
+	public static final int PALEGOLDENROD = 0xFFEEE8AA;
+	public static final int PALEGREEN = 0xFF98FB98;
+	public static final int PALETURQUOISE = 0xFFAFEEEE;
+	public static final int PALEVIOLETRED = 0xFFDB7093;
+	public static final int PAPAYAWHIP = 0xFFFFEFD5;
+	public static final int PEACHPUFF = 0xFFFFDAB9;
+	public static final int PERU = 0xFFCD853F;
+	public static final int PINK = 0xFFFFC0CB;
+	public static final int PLUM = 0xFFDDA0DD;
+	public static final int POWDERBLUE = 0xFFB0E0E6;
+	public static final int PURPLE = 0xFF800080;
+	public static final int RED = 0xFFFF0000;
+	public static final int ROSYBROWN = 0xFFBC8F8F;
+	public static final int ROYALBLUE = 0xFF4169E1;
+	public static final int SADDLEBROWN = 0xFF8B4513;
+	public static final int SALMON = 0xFFFA8072;
+	public static final int SANDYBROWN = 0xFFF4A460;
+	public static final int SEAGREEN = 0xFF2E8B57;
+	public static final int SEASHELL = 0xFFFFF5EE;
+	public static final int SIENNA = 0xFFA0522D;
+	public static final int SILVER = 0xFFC0C0C0;
+	public static final int SKYBLUE = 0xFF87CEEB;
+	public static final int SLATEBLUE = 0xFF6A5ACD;
+	public static final int SLATEGRAY = 0xFF708090;
+	public static final int SNOW = 0xFFFFFAFA;
+	public static final int SPRINGGREEN = 0xFF00FF7F;
+	public static final int STEELBLUE = 0xFF4682B4;
+	public static final int TAN = 0xFFD2B48C;
+	public static final int TEAL = 0xFF008080;
+	public static final int THISTLE = 0xFFD8BFD8;
+	public static final int TOMATO = 0xFFFF6347;
+	public static final int TRANSPARENT = 0x00000000;
+	public static final int TURQUOISE = 0xFF40E0D0;
+	public static final int VIOLET = 0xFFEE82EE;
+	public static final int WHEAT = 0xFFF5DEB3;
+	public static final int WHITE = 0xFFFFFFFF;
+	public static final int WHITESMOKE = 0xFFF5F5F5;
+	public static final int YELLOW = 0xFFFFFF00;
+	public static final int YELLOWGREEN = 0xFF9ACD32;
 
-	private UEHtmlColor(int color) {
-		this.color = color;
-	}
-
-	public int getColor() {
-		return color;
+	private UEHtmlColor(){
 	}
 }
