dff7ce5cee031d58c623f07962c7a95f2e3d3b21
Soniy7x <soniy7x@gmail.com>
2014-11-23 15:58:35 +0800

    Update - UEMethod支持dp2px返回浮点型

--- Auie/src/org/auie/utils/UEMethod.java
+++ Auie/src/org/auie/utils/UEMethod.java
@@ -20,4 +20,14 @@ public final class UEMethod {
 	public static int dp2px(Context context, float dp){
         return (int) (dp * context.getResources().getDisplayMetrics().density + 0.5f);
     }
+	
+	/**
+	 * DP 转化 PX (Convert DP to PX)
+	 * @param context 上下文(Context or Activity)
+	 * @param dp DP大小(number of DP)
+	 * @return int PX大小(number of PX)
+	 */
+	public static float dp2pxReturnFloat(Context context, float dp){
+        return dp * context.getResources().getDisplayMetrics().density + 0.5f;
+    }
 }
