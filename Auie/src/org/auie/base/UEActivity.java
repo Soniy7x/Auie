770f473c8fc78295c26d3dd8c018d56af82f6a64
Soniy7x <soniy7x@gmail.com>
2014-11-26 21:04:47 +0800

    Update - UEActivity更新Wiki

--- Auie/src/org/auie/base/UEActivity.java
+++ Auie/src/org/auie/base/UEActivity.java
@@ -1,22 +1,23 @@
 package org.auie.base;
 
 import org.auie.annotation.UEAnnotationManager;
+import org.auie.utils.UEDevice;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 
 /**
  * 
- * 基础类(Base Class)
+ * 基础类
  * 
- * AUIE框架入口，自定义的Activity继承UEActivity后，才可以使用UEAnnotation注释自动初始化布局、控件和对象
- * (If you want to use "AUIE", then a custom Activity must extend from UEActivity, it's the 
- * only entrance frame, it can only be used within the framework of the custom annotations, 
- * realize the automatic layout controls the initialization)
+ * AUIE框架入口，自定义的Activity继承UEActivity后，才可以使用UEAnnotation注释自动初始化布局、控件和对象=
  * 
  * @author Soniy7x
  * 
@@ -24,12 +25,10 @@ import android.view.View.OnClickListener;
 public abstract class UEActivity extends Activity implements OnClickListener{
 
 	//全局Activity对象，可当作context使用
-	//(Global Activity Object, can be used as Context)
 	public final Activity activity = this;
 	
 	/**
 	 * 复写onCreat方法，调用初始化入口方法，严禁复写
-	 * (calling initialization entrance method, it's strictly prohibited to override)
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +40,6 @@ public abstract class UEActivity extends Activity implements OnClickListener{
 
 	/**
 	 * 初始化开始方法
-	 * (Initialization begins)
 	 */
 	private void initializeBegin() {
 		UEAnnotationManager.getInstance().initialize(this, true);
@@ -49,16 +47,18 @@ public abstract class UEActivity extends Activity implements OnClickListener{
 	
 	/**
 	 * 初始化准备方法
-	 * (Initialization prepares, here you can perform some method)
 	 */
+	@TargetApi(Build.VERSION_CODES.KITKAT)
 	protected void initializePrepare() {
-		
+		if (UEDevice.getOSVersionCode() >= Build.VERSION_CODES.KITKAT) {
+			getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+			getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+		}
 	}
 	
 	/**
 	 * 自定义字体方法
-	 * (Use this method that set widget's font)
-	 * @param typeface 自定义字体(customer font)
+	 * @param typeface 自定义字体
 	 */
 	protected void initializeFont(Typeface typeface) {
 		UEAnnotationManager.getInstance().initializeFont(activity, typeface);
@@ -66,16 +66,14 @@ public abstract class UEActivity extends Activity implements OnClickListener{
 	
 	/**
 	 * 实现OnClickListener接口
-	 * (Perform onClickListener)
 	 */
 	@Override
 	public void onClick(View v) {}
 	
 	/**
 	 * 无传递参数简单跳转Activity方法
-	 * (Form this Activity to other Activity, it's not incidental parameters)
-	 * @param clazz 将要跳转的目的地(Other Activity - Destination)
-	 * @param isClose 完成后是否关闭此类(Whether to close the current Activity)
+	 * @param clazz 将要跳转的目的地
+	 * @param isClose 完成后是否关闭此类
 	 */
 	public void startActivity(Class<?> clazz, boolean isClose){
 		startActivity(new Intent(this, clazz));
@@ -86,7 +84,6 @@ public abstract class UEActivity extends Activity implements OnClickListener{
 	
 	/**
 	 * 初始化完成方法，必须实现
-	 * (Initialization is complete method that must be implemented)
 	 */
 	protected abstract void initializeFinish();
 	
